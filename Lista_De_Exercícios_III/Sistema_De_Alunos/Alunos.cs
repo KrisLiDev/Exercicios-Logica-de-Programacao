3ebb5161d617ae21bce390865e087a2d1867c860
KrisLi <krislidev@gmail.com>
2026-06-10 06:30:19 -0300

    Estrutura inicial para a atividade 4 - Sistema de Alunos

---
+++ Lista_De_Exercícios_III/Sistema_De_Alunos/Alunos.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema_De_Alunos
+{
+    public class Alunos
+    {
+        public string Nome;
+        public float Nota1;
+        public float Nota2;
+    }
+}
